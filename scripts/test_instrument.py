5aca9a9758c3be7c2c45bf7471fbe57367af732a
Grigory Sharov <sharov.grigory@gmail.com>
2022-04-16 16:10:03 +0100

    wip

--- scripts/test_instrument.py
+++ scripts/test_instrument.py
@@ -7,23 +7,23 @@ from temscript.utils.enums import *
 def test_projection(microscope, eftem=False):
     print("Testing projection...")
     projection = microscope.optics.projection
-    print("Mode:", projection.mode)
-    print("Focus:", projection.focus)
-    print("Magnification:", projection.magnification)
-    print("MagnificationIndex:", projection.magnificationIndex)
-    print("CameraLengthIndex:", projection.camera_length_index)
-    print("ImageShift:", projection.image_shift)
-    print("ImageBeamShift:", projection.image_beam_shift)
-    print("DiffractionShift:", projection.diffraction_shift)
-    print("DiffractionStigmator:", projection.diffraction_stigmator)
-    print("ObjectiveStigmator:", projection.objective_stigmator)
-    print("SubMode:", projection.magnification_range)
-    print("LensProgram:", projection.is_eftem_on)
-    print("ImageRotation:", projection.image_rotation)
-    # print("DetectorShift:", projection.DetectorShift)
-    # print("DetectorShiftMode:", projection.DetectorShiftMode)
-    print("ImageBeamTilt:", projection.image_beam_tilt)
-    print("LensProgram:", projection.is_eftem_on)
+    print("\tMode:", projection.mode)
+    print("\tFocus:", projection.focus)
+    print("\tMagnification:", projection.magnification)
+    print("\tMagnificationIndex:", projection.magnificationIndex)
+    print("\tCameraLengthIndex:", projection.camera_length_index)
+    print("\tImageShift:", projection.image_shift)
+    print("\tImageBeamShift:", projection.image_beam_shift)
+    print("\tDiffractionShift:", projection.diffraction_shift)
+    print("\tDiffractionStigmator:", projection.diffraction_stigmator)
+    print("\tObjectiveStigmator:", projection.objective_stigmator)
+    print("\tSubMode:", projection.magnification_range)
+    print("\tLensProgram:", projection.is_eftem_on)
+    print("\tImageRotation:", projection.image_rotation)
+    # print("\tDetectorShift:", projection.DetectorShift)
+    # print("\tDetectorShiftMode:", projection.DetectorShiftMode)
+    print("\tImageBeamTilt:", projection.image_beam_tilt)
+    print("\tLensProgram:", projection.is_eftem_on)
 
     projection.reset_defocus()
 
@@ -43,14 +43,14 @@ def test_acquisition(microscope):
                                               size=AcqImageSize.FULL,
                                               exp_time=0.25,
                                               binning=2)
-        print("Image name:", image.name)
-        print("Image size:", image.width, image.height)
-        print("Bit depth:", image.bit_depth)
+        print("\tImage name:", image.name)
+        print("\tImage size:", image.width, image.height)
+        print("\tBit depth:", image.bit_depth)
 
         if image.metadata is not None:
-            print("Binning:", image.metadata['Binning.Width'])
-            print("Exp time:", image.metadata['ExposureTime'])
-            print("Timestamp:", image.metadata['TimeStamp'])
+            print("\tBinning:", image.metadata['Binning.Width'])
+            print("\tExp time:", image.metadata['ExposureTime'])
+            print("\tTimestamp:", image.metadata['TimeStamp'])
 
         fn = cam_name + ".mrc"
         print(f"Saving to {fn}")
@@ -69,10 +69,10 @@ def test_acquisition(microscope):
 def test_vacuum(microscope, full_test=False):
     print("Testing vacuum...")
     vacuum = microscope.vacuum
-    print("Status:", vacuum.status)
-    print("PVPRunning:", vacuum.is_buffer_running)
-    print("ColumnValvesOpen:", vacuum.is_colvalves_open)
-    print("Gauges:", vacuum.gauges)
+    print("\tStatus:", vacuum.status)
+    print("\tPVPRunning:", vacuum.is_buffer_running)
+    print("\tColumnValvesOpen:", vacuum.is_colvalves_open)
+    print("\tGauges:", vacuum.gauges)
 
     if full_test:
         vacuum.colvalves_open()
@@ -107,10 +107,10 @@ def test_stage(microscope, do_move=False):
     print("Testing stage...")
     stage = microscope.stage
     pos = stage.position
-    print("Status:", stage.status)
-    print("Position:", pos)
-    print("Holder:", stage.holder_type)
-    print("Limits:", stage.limits)
+    print("\tStatus:", stage.status)
+    print("\tPosition:", pos)
+    print("\tHolder:", stage.holder_type)
+    print("\tLimits:", stage.limits)
 
     if not do_move:
         return
@@ -127,21 +127,21 @@ def test_stage(microscope, do_move=False):
     print("\tPosition:", stage.position)
 
 
-def test_cameras(microscope):
+def test_detectors(microscope):
     print("Testing cameras...")
     dets = microscope.detectors
-    print("Film settings:", dets.film_settings)
-    print("Cameras:", dets.cameras)
-    print("STEM detectors:", dets.stem_detectors)
+    print("\tFilm settings:", dets.film_settings)
+    print("\tCameras:", dets.cameras)
+    print("\tSTEM detectors:", dets.stem_detectors)
 
 
 def test_optics(microscope):
     print("Testing optics...")
     opt = microscope.optics
-    print("ScreenCurrent:", opt.screen_current)
-    print("BeamBlanked:", opt.is_beam_blanked)
-    print("AutoNormalizeEnabled:", opt.is_autonormalize_on)
-    print("ShutterOverrideOn:", opt.is_shutter_override_on)
+    print("\tScreenCurrent:", opt.screen_current)
+    print("\tBeamBlanked:", opt.is_beam_blanked)
+    print("\tAutoNormalizeEnabled:", opt.is_autonormalize_on)
+    print("\tShutterOverrideOn:", opt.is_shutter_override_on)
     opt.beam_blank()
     opt.beam_unblank()
     opt.normalize(ProjectionNormalization.OBJECTIVE)
@@ -151,76 +151,76 @@ def test_optics(microscope):
 def test_illumination(microscope):
     print("Testing illumination...")
     illum = microscope.optics.illumination
-    print("Mode:", illum.mode)
-    print("SpotsizeIndex:", illum.spotsize)
-    print("Intensity:", illum.intensity)
-    print("IntensityZoomEnabled:", illum.intensity_zoom)
-    print("IntensityLimitEnabled:", illum.intensity_limit)
-    print("Shift:", illum.beam_shift)
-    print("Tilt:", illum.beam_tilt)
-    print("RotationCenter:", illum.rotation_center)
-    print("CondenserStigmator:", illum.condenser_stigmator)
-    print("DFMode:", illum.dark_field_mode)
+    print("\tMode:", illum.mode)
+    print("\tSpotsizeIndex:", illum.spotsize)
+    print("\tIntensity:", illum.intensity)
+    print("\tIntensityZoomEnabled:", illum.intensity_zoom)
+    print("\tIntensityLimitEnabled:", illum.intensity_limit)
+    print("\tShift:", illum.beam_shift)
+    print("\tTilt:", illum.beam_tilt)
+    print("\tRotationCenter:", illum.rotation_center)
+    print("\tCondenserStigmator:", illum.condenser_stigmator)
+    print("\tDFMode:", illum.dark_field_mode)
 
     if microscope.condenser_system == CondenserLensSystem.THREE_CONDENSER_LENSES:
-        print("CondenserMode:", illum.condenser_mode)
-        print("IlluminatedArea:", illum.illuminated_area)
-        print("ProbeDefocus:", illum.probe_defocus)
-        print("ConvergenceAngle:", illum.convergence_angle)
-        print("C3ImageDistanceParallelOffset:", illum.C3ImageDistanceParallelOffset)
+        print("\tCondenserMode:", illum.condenser_mode)
+        print("\tIlluminatedArea:", illum.illuminated_area)
+        print("\tProbeDefocus:", illum.probe_defocus)
+        print("\tConvergenceAngle:", illum.convergence_angle)
+        print("\tC3ImageDistanceParallelOffset:", illum.C3ImageDistanceParallelOffset)
 
 
 def test_stem(microscope):
     print("Testing STEM...")
     stem = microscope.stem
-    print("StemAvailable:", stem.is_stem_available)
+    print("\tStemAvailable:", stem.is_stem_available)
 
     if stem.is_stem_available:
         stem.enable()
-        print("Illumination.StemMagnification:", stem.stem_magnification)
-        print("Illumination.StemRotation:", stem.stem_rotation)
-        print("Illumination.StemFullScanFieldOfView:", stem.stem_scan_fov)
+        print("\tIllumination.StemMagnification:", stem.stem_magnification)
+        print("\tIllumination.StemRotation:", stem.stem_rotation)
+        print("\tIllumination.StemFullScanFieldOfView:", stem.stem_scan_fov)
         stem.disable()
 
 
 def test_gun(microscope):
     print("Testing gun...")
     gun = microscope.gun
-    print("HTState:", gun.ht_state)
-    print("HTValue:", gun.voltage)
-    print("HTMaxValue:", gun.voltage_max)
-    print("Shift:", gun.shift)
-    print("Tilt:", gun.tilt)
+    print("\tHTState:", gun.ht_state)
+    print("\tHTValue:", gun.voltage)
+    print("\tHTMaxValue:", gun.voltage_max)
+    print("\tShift:", gun.shift)
+    print("\tTilt:", gun.tilt)
 
 
 def test_apertures(microscope):
     print("Testing apertures...")
     aps = microscope.apertures
-    print("GetCurrentPresetPosition", aps.vpp_position)
+    print("\tGetCurrentPresetPosition", aps.vpp_position)
     aps.vpp_next_position()
 
 
 def test_general(microscope, check_door=False):
     print("Testing configuration...")
 
-    print("Configuration.ProductFamily:", microscope.family)
-    print("UserButtons:", microscope.user_buttons)
-    print("BlankerShutter.ShutterOverrideOn:",
+    print("\tConfiguration.ProductFamily:", microscope.family)
+    print("\tUserButtons:", microscope.user_buttons)
+    print("\tBlankerShutter.ShutterOverrideOn:",
           microscope.optics.is_shutter_override_on)
-    print("Condenser system:", microscope.condenser_system)
-    print("Licenses:", microscope.check_license())
+    print("\tCondenser system:", microscope.condenser_system)
+    print("\tLicenses:", microscope.check_license())
 
     if check_door:
-        print("User door:", microscope.user_door.state)
+        print("\tUser door:", microscope.user_door.state)
 
 
 if __name__ == '__main__':
-    print("Starting Test...")
+    print("Starting tests...")
 
     full_test = False
     microscope = Microscope()
     test_projection(microscope, eftem=True)
-    test_cameras(microscope)
+    test_detectors(microscope)
     test_vacuum(microscope, full_test=full_test)
     test_autoloader(microscope, full_test=full_test, slot=1)
     test_temperature(microscope)
